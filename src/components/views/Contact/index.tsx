dde8245e516f44902175a49ba3cfb08ec5ad9c97
SpykeRel04D <roger.vidal.lloveras@gmail.com>
2021-02-01 03:42:43 +0100

    build mode

--- src/components/views/Contact/index.tsx
+++ src/components/views/Contact/index.tsx
@@ -4,7 +4,8 @@ export default function Contact() {
     return(
         <div className={`${styles.contact} section`}>
             <h2 className={`${styles.contactTitle} sectionTitle`}>CONTACT</h2>
-            <form className={styles.contactForm}>
+            <form className={styles.contactForm} name="contact" method="POST" data-netlify="true">
+                <input type="hidden" name="form-name" value="contact" />
                 <input type="text" id="fname" name="firstname" placeholder="Your name.." />
                 <input type="text" id="email" name="contactmail" placeholder="Your mail.." />
                 <textarea id="subject" name="subject" rows={10} placeholder="Write something.." />
